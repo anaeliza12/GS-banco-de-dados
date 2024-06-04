acba06381af2220ab9fa2d4e58a8cbf6265e977b
Ana Eliza <anaelizaperobelli@gmail.com>
2024-06-04 17:34:30 -0300

    Ex2, creating synonym for other user tables

---
+++ GS_3SIS_EX2.sql
@@ -0,0 +1,6 @@
+SELECT * FROM pf0645.localizacoes;
+SELECT * FROM pf0645.especies_marinhas;
+SELECT * FROM pf0645.condicoes_ambientais;
+SELECT * FROM pf0645.poluicoes;
+
+CREATE SYNONYM localizacao FOR pf0645.localizacoes
