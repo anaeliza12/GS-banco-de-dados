1f7527e1eab6a661a2a644ea501f94116ee050e7
Ana Eliza <anaelizaperobelli@gmail.com>
2024-06-04 17:35:43 -0300

    Ex2, creating synonym for other user tables

--- GS_3SIS_EX2.sql
+++ GS_3SIS_EX2.sql
@@ -3,4 +3,4 @@ SELECT * FROM pf0645.especies_marinhas;
 SELECT * FROM pf0645.condicoes_ambientais;
 SELECT * FROM pf0645.poluicoes;
 
-CREATE SYNONYM especies_marinhas FOR pf0645.especies_marinhas
+CREATE SYNONYM condicoes_ambientais FOR pf0645.condicoes_ambientais
